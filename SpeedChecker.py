2dbc05e36b85f31527a99f08022a2042bf56cd18
Rishit-1010 <siliconsmarketing.123@gmail.com>
2026-08-22 17:20:28 +0530

    Add files via upload

---
+++ SpeedChecker.py
@@ -0,0 +1,14 @@
+def check_speed():
+    s=int(input("Enter your speed(in km/hr): "))
+    if s<=70:
+        print("OK")
+    if s>70:
+        d=0
+        for i in range(71,s+1,5):
+            d+=1
+        if 5<d<11:
+            print("WARNING!")
+        elif d>10:
+            print("Lisence Suspended!")
+        print("Demerit Points:",d)
+check_speed()

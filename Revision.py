ebb71e918e5a85f86a93f4de0f2fbb8d137ae71f
Rishit-1010 <siliconsmarketing.123@gmail.com>
2026-09-02 13:09:02 +0530

    Add files via upload

---
+++ Revision.py
@@ -0,0 +1,32 @@
+def sum_digits(a):
+    while a>0:
+        d=a%10
+        s=s+d
+        a=a//10
+    return s
+def is_prime(a):
+    flag=False
+    i=2
+    while(i<a):
+        if a%i==0:
+            flag=True
+            break
+        i+=1
+    if flag:
+        return False
+    else:
+        return True
+def is_pallindrome(a):
+    n=a
+    while n>0:
+        d=n%10
+        rev=(rev*10)+d
+        n//=10
+    if rev==a:
+        return True
+    else: 
+        return False
+a=int(input("Enter a no.: "))
+print(sum_digits(a))
+print(is_prime(a))
+print(is_pallindrome(a))

23a4772d9cb0311551d707dbb5b49bc2d584937c
Rishit-1010 <siliconsmarketing.123@gmail.com>
2026-09-04 09:12:37 +0530

    Add files via upload

---
+++ Triangle3.py
@@ -0,0 +1,8 @@
+a=int(input("Enter no. of rows required:"))
+for i in range(1,a+1):
+     print((a-i)*" ", end="") 
+     for j in range(1,i+1):
+          print(j, end="")
+     for k in range(i-1,0,-1):
+          print(k, end="")
+     print()   

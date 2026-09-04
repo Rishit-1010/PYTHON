df18d011c9bbd93d03fae9c80158dff33d2607e5
Rishit-1010 <siliconsmarketing.123@gmail.com>
2026-09-04 19:35:49 +0530

    Add files via upload

--- Triangle2.py
+++ Triangle2.py
@@ -1,10 +1,7 @@
 def triangle2(a):
     for i in range(1,a+1):
         for j in range(i,0,-1):
-            if j%2==0:
-                print(0,end="")
-            else:
-                print(1,end="")
+            print(j%2,end="")
         print()            
 a=int(input("Enter no. of rows:"))    
 triangle2(a)            

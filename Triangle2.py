acc7e720e101ae20a05e123b965436b24bbcbea1
Rishit-1010 <siliconsmarketing.123@gmail.com>
2026-09-06 09:39:45 +0530

    Refactor triangle2 function and clean up code
    Refactor triangle2 function to improve output formatting and remove commented-out code.

--- Triangle2.py
+++ Triangle2.py
@@ -2,6 +2,14 @@ def triangle2(a):
     for i in range(1,a+1):
         for j in range(i,0,-1):
             print(j%2,end="")
-        print()            
-a=int(input("Enter no. of rows:"))    
-triangle2(a)            
+        print()
+a=int(input("Enter no. of rows:"))
+triangle2(a)
+# def triangle2(a):
+#     for i in range(1,a+1):
+#         print((a-i)*" ", end="")
+#         for j in range(i,0,-1):
+#             print(j%2,end="")
+#         print()
+# a=int(input("Enter no. of rows:"))
+# triangle2(a)
